6ca66ca8c827e4b7c295dd8528a347d1a75b4c6f
se7enmfk <se7enmfk2@outlook.com>
2018-12-29 16:37:12 +0800

    解密时添加utf8

--- spring-boot-starter-encrypt/src/main/java/com/cxytiandi/encrypt/util/AesEncryptUtils.java
+++ spring-boot-starter-encrypt/src/main/java/com/cxytiandi/encrypt/util/AesEncryptUtils.java
@@ -1,56 +1,60 @@
 package com.cxytiandi.encrypt.util;
 
+import org.apache.commons.codec.binary.Base64;
+
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.spec.SecretKeySpec;
-import org.apache.commons.codec.binary.Base64;
 
 public class AesEncryptUtils {
-	private static final String KEY = "d7b85f6e214abcda";
-	private static final String ALGORITHMSTR = "AES/ECB/PKCS5Padding";
+    private static final String KEY = "d7b85f6e214abcda";
+    private static final String ALGORITHM_STR = "AES/ECB/PKCS5Padding";
+    private static final String UTF8 = "utf-8";
+    private static final String AES = "AES";
+    private static final int KEY_SIZE = 128;
 
-	public static String base64Encode(byte[] bytes) {
-		return Base64.encodeBase64String(bytes);
-	}
+    public static String base64Encode(byte[] bytes) {
+        return Base64.encodeBase64String(bytes);
+    }
 
-	public static byte[] base64Decode(String base64Code) throws Exception {
-		return Base64.decodeBase64(base64Code);
-	}
+    public static byte[] base64Decode(String base64Code) throws Exception {
+        return Base64.decodeBase64(base64Code);
+    }
 
-	public static byte[] aesEncryptToBytes(String content, String encryptKey) throws Exception {
-		KeyGenerator kgen = KeyGenerator.getInstance("AES");
-		kgen.init(128);
-		Cipher cipher = Cipher.getInstance(ALGORITHMSTR);
-		cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(encryptKey.getBytes(), "AES"));
-		return cipher.doFinal(content.getBytes("utf-8"));
-	}
+    public static byte[] aesEncryptToBytes(String content, String encryptKey) throws Exception {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance(AES);
+        keyGenerator.init(KEY_SIZE);
+        Cipher cipher = Cipher.getInstance(ALGORITHM_STR);
+        cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(encryptKey.getBytes(), AES));
+        return cipher.doFinal(content.getBytes(UTF8));
+    }
 
-	public static String aesEncrypt(String content, String encryptKey) throws Exception {
-		return base64Encode(aesEncryptToBytes(content, encryptKey));
-	}
+    public static String aesEncrypt(String content, String encryptKey) throws Exception {
+        return base64Encode(aesEncryptToBytes(content, encryptKey));
+    }
 
-	public static String aesDecryptByBytes(byte[] encryptBytes, String decryptKey) throws Exception {
-		KeyGenerator kgen = KeyGenerator.getInstance("AES");
-		kgen.init(128);
-		Cipher cipher = Cipher.getInstance(ALGORITHMSTR);
-		cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(decryptKey.getBytes(), "AES"));
-		byte[] decryptBytes = cipher.doFinal(encryptBytes);
-		return new String(decryptBytes);
-	}
+    public static String aesDecryptByBytes(byte[] encryptBytes, String decryptKey) throws Exception {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance(AES);
+        keyGenerator.init(KEY_SIZE);
+        Cipher cipher = Cipher.getInstance(ALGORITHM_STR);
+        cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(decryptKey.getBytes(), AES));
+        byte[] decryptBytes = cipher.doFinal(encryptBytes);
+        return new String(decryptBytes, UTF8);
+    }
 
-	public static String aesDecrypt(String encryptStr, String decryptKey) throws Exception {
-		return aesDecryptByBytes(base64Decode(encryptStr), decryptKey);
-	}
+    public static String aesDecrypt(String encryptStr, String decryptKey) throws Exception {
+        return aesDecryptByBytes(base64Decode(encryptStr), decryptKey);
+    }
 
-	public static void main(String[] args) throws Exception {
-		String content = "你好";
-		System.out.println("加密前：" + content);
+    public static void main(String[] args) throws Exception {
+        String content = "你好";
+        System.out.println("加密前：" + content);
 
-		String encrypt = aesEncrypt(content, KEY);
-		System.out.println(encrypt.length() + ":加密后：" + encrypt);
+        String encrypt = aesEncrypt(content, KEY);
+        System.out.println(encrypt.length() + ":加密后：" + encrypt);
 
-		String decrypt = aesDecrypt(encrypt, KEY);
-		System.out.println("解密后：" + decrypt);
-	}
-	
+        String decrypt = aesDecrypt(encrypt, KEY);
+        System.out.println("解密后：" + decrypt);
+    }
+
 }
